07b9c8291c37e584df4218128498357747c55b9e
Hans Magnus Inderberg <hinderberg@gmail.com>
2023-12-15 14:07:31 +0100

    update to new config

--- startup.sh
+++ startup.sh
@@ -1,14 +1,23 @@
 #!/usr/bin/env bash
-
-cp ./.gitconfig ~/.gitconfig
-cp ./.npmrc ~/.npmrc
-cp ./.zshrc ~/.zshrc
-cp -R ./.hammerspoon ~/.hammerspoon
-
 if [[ `uname` == "Darwin" ]]; then
+	cp ./.gitconfig ~/.gitconfig
+	cp ./.npmrc ~/.npmrc
+	cp ./.zshrc ~/.zshrc
+	cp -R ./.hammerspoon ~/.hammerspoon
+
+	mkdir -p ~/.1password && ln -s ~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock ~/.1password/agent.sock
+	cp -R ./.ssh ~/.ssh
+
+	mkdir -p ~/coding-personal
+	cp ./.gitconfig.personal ~/coding-personal/.gitconfig
+	mkdir -p ~/coding-work/.gitconfig.work
+	cp ./.gitconfig.work ~/coding-work/.gitconfig
+
 	echo "Updating OSX settings and installing apps"
 	xcode-select --install
 	sh ./.programs
-fi
 
-echo "Finished"
+	echo "Finished"
+else
+	echo "Not OSX"
+fi
